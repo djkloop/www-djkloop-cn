dd0f8e20ba15d3e32791ebed5fcc2dc9c855922c
djkloop <1044032256@qq.com>
2018-04-21 14:59:42 +0800

    feat(all): init git

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const app = new Koa();
+const router = new Router();
+const PORT = '36666';
+
+router.get('/', (ctx, next) => {
+  ctx.body = "我是首页"
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(PORT, () => {
+  console.log(`server is runing ${PORT}!`);
+});
